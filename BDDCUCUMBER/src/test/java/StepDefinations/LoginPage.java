0a208fc2a41d01af6291f9402cb3e8f0b1fec3a6
insightinfy <namratakul123@gmail.com>
2023-11-05 14:24:39 +0530

    Login with Parameterization

--- BDDCUCUMBER/src/test/java/StepDefinations/LoginPage.java
+++ BDDCUCUMBER/src/test/java/StepDefinations/LoginPage.java
@@ -14,12 +14,14 @@ import io.cucumber.java.en.When;
 public class LoginPage {
 	
 	WebDriver driver=new ChromeDriver();
-	
+
 	@Given("i am on login page")
 	public void i_am_on_login_page() {
 		driver.navigate().to("https://practicetestautomation.com/practice-test-login/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	    
 	}
+
 
 	@When("^user enters(.*) and(.*)$")
 	public void user_enters_and(String username, String password) {
